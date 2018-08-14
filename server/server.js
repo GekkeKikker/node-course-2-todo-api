ac03d5fc583bf0094fbb9288e1f6663e2bf04bec
GekkeKikker <maarten.alles@pandora.be>
2018-08-14 16:21:54 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,39 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+// GET /todos/AZAZAZRERDF2232321
+app.get('/todos/:id', (req, res) => {
+  id = req.params.id;
+
+//validate id using isValid
+  if(!ObjectID.isValid(id)) {
+  //404 - send back empty send
+    res.status(404).send();
+    return console.log('Id not valid');
+  }
+
+  //findById
+  Todo.findById(id).then((todo) => {
+    //success
+    if(!todo) {
+      //if no todo - send 404 with empty body
+      res.status(404).send();
+      return console.log('Id not found');
+    }
+    //if todo - send it back
+    res.send({todo});
+    console.log(JSON.stringify(todo, undefined, 2));
+
+    //error
+  }).catch((e) => {
+    //400 - and send empty body back
+    res.status(400).send();
+    console.log(e);
+  });
+});
+
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
