b4ef3bdd9f759cd70a45eca916ea61742649aee9
GekkeKikker <maarten.alles@pandora.be>
2018-08-15 11:39:12 +0200

    test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id', (req, res) => {
 
     res.send({todo});
   }).catch((e) => {
-    res.status(400).send();
+    res.status(404).send();
   });
 });
 
@@ -62,7 +62,7 @@ app.delete('/todos/:id', (req, res) => {
 
     res.send({todo});
   }).catch((e) => {
-    res.status(400).send();
+    res.status(404).send();
   });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -85,7 +85,7 @@ describe('POST /todos', () => {
 
     it('should return 404 if todo not found', (done) => {
       //make sure you get a 404 back
-      var hexId = new ObjectID.toHexString();
+      var hexId = new ObjectID().toHexString();
 
       request(app)
         .get('/todos/${hexId}')
@@ -97,6 +97,47 @@ describe('POST /todos', () => {
       // /todos/123
       request(app)
         .get('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+  });
+
+  describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+      var hexId = todos[1]._id.toHexString();
+
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+        // query database using findById toNotExist
+        // expect(null).toNotExist();
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+        });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+      var hexId = new ObjectID().toHexString();
+
+      request(app)
+        .delete('/todos/${hexId}')
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if object id is not valid', (done) => {
+      request(app)
+        .delete('/todos/123')
         .expect(404)
         .end(done);
     });
